a07205752da8c3f8c709e99d80c329054e6ab179
Rafal <rafal.z.malucha@gmail.com>
2025-03-18 13:43:55 +0100

    add support for existing features on linux

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -3,6 +3,8 @@
 #include <iomanip>
 #include <sstream>
 
+#ifdef _WIN32
+
 std::string currentDateTime() {
     std::time_t t = std::time(nullptr);
     std::tm tm;
@@ -11,3 +13,16 @@ std::string currentDateTime() {
     oss << std::put_time(&tm, "%Y-%m-%d %H:%M:%S");
     return oss.str();
 }
+
+#else
+
+std::string currentDateTime() {
+    std::time_t t = std::time(nullptr);
+    std::tm tm;
+    localtime_r(&t, &tm);
+    std::ostringstream oss;
+    oss << std::put_time(&tm, "%Y-%m-%d %H:%M:%S");
+    return oss.str();
+}
+
+#endif
