389aab4c8dc8822355c8363132fd5ad9b702def8
Rafal <rafal.z.malucha@gmail.com>
2025-03-29 19:21:04 +0100

    refactor and test window manager

--- src/WindowManager.h
+++ src/WindowManager.h
@@ -4,10 +4,16 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <tuple>
+
 GLFWwindow* createWindow(int width, int height, const char* title);
 
 void closeWindow(GLFWwindow* window, const char* title);
 
 GLFWwindow* initializeEngineWindow();
+
+GLFWmonitor* getPrimaryMonitor();
+
+std::tuple<int, int> calculateWindowResolution(GLFWmonitor* primary);
 
 #endif // WINDOWMANAGER_H
---
+++ tests/testWindowManager.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+
+#include "WindowManager.h"
+
+TEST(WindowManagerTest, initializeEngineWindow_check) {
+    GLFWwindow* window = initializeEngineWindow();
+    ASSERT_NE(window, nullptr);
+}
+
+TEST(WindowManagerTest, getPrimaryMonitor_test) {
+
+    GLFWmonitor* primary = getPrimaryMonitor();
+    ASSERT_NE(primary, nullptr);
+}
+
+TEST(WindowManagerTest, calculateWindowResolution_test) {
+    GLFWmonitor* primary = getPrimaryMonitor();
+    auto [width, height] = calculateWindowResolution(primary);
+    ASSERT_GT(width, 0);
+    ASSERT_GT(height, 0);
+}
+
+// clean up after tests
+
+TEST(WindowManagerTest, cleanUp) {
+    glfwTerminate();
+}
